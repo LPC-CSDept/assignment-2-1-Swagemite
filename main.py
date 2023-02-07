228c0c2328d2d8b2f1d15be4dfcd73f64c9b0564
Brent Young <bryoung777@icloud.com>
2023-02-07 15:55:30 -0800

    Attempt 2

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ def main():
     # Comlete your code here
     num_males = int(input("Enter number of males: "))
     num_females = int(input("Enter number of females: "))
+    percM = num_males / (num_males + num_females) *100
+    percF = num_females / (num_males + num_females) * 100
     total = num_males + num_females
     print(total)
     print(num_males, num_females)
-    print(f'Percentage of males: \t {num_males:.2f}%')
-    print(f'Percentage of females: \t {num_females:.2f}%')
+    print(f'Percentage of males: \t {percM:.2f}%')
+    print(f'Percentage of females: \t {percF:.2f}%')
     ##################################################
     pass
 
