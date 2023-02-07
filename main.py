df4ebc480302a8846956e9da592b309e8463a3eb
Brent Young <bryoung777@icloud.com>
2023-02-07 15:50:38 -0800

    Attempt 1

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 def main():
     ##################################################
     # Comlete your code here
+    num_males = int(input("Enter number of males: "))
+    num_females = int(input("Enter number of females: "))
+    total = num_males + num_females
+    print(total)
+    print(num_males, num_females)
+    print(f'Percentage of males: \t {num_males:.2f}%')
+    print(f'Percentage of females: \t {num_females:.2f}%')
     ##################################################
     pass
 
