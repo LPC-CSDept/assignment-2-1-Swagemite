2752daddbc7a1c5aa800f5e86904519dd90c5bcf
Brent Young <bryoung777@icloud.com>
2023-02-07 16:35:25 -0800

    Attempt 3

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ def main():
     total = num_males + num_females
     print('The total number of student:', total)
     print('The number of males and femmales:',num_males, num_females)
-    print(f'Percentage of males : \t {percM:.2f}%')
-    print(f'Percentage of females: \t {percF:.2f}%')
+    print(f'Percentage of males and females: \t {percM:.2f}% \t {percF:.2f}%')
     ##################################################
     pass
 
